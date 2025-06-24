18509b7038b9c388deea685f7fb75e599ff6313a
CeriXXIX <beyondskyvalley@gmail.com>
2025-06-24 18:12:13 -0400

    Amended code.

--- src/main/java/com/ceri/amberheart/item/custom/HymnalBatonItem.java
+++ src/main/java/com/ceri/amberheart/item/custom/HymnalBatonItem.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class HymnalBatonItem extends Item {
     public HymnalBatonItem(Properties properties) {
@@ -14,7 +15,7 @@ public class HymnalBatonItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, @NotNull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
         if (!level.isClientSide) {
             player.sendSystemMessage(Component.literal("The baton hums softly..."));
