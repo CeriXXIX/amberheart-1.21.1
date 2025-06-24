c2019d9822d7e77ff76b1f6227c4932880a64a26
CeriXXIX <beyondskyvalley@gmail.com>
2025-06-24 18:55:57 -0400

    Created placeholder class for Angelic Etchings block.

---
+++ src/main/java/com/ceri/amberheart/block/custom/AngelicEtchingsBlock.java
@@ -0,0 +1,14 @@
+//package com.ceri.amberheart.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+//public class AngelicEtchingsBlock extends Block {
+//    public AngelicEtchingsBlock(Properties properties) {
+//        super(properties);
+//    }
+//}
+
+
+
+// Eventually I'd like to set up a custom interactable block here that triggers a GUI for reading ancient texts.
+// I have no idea how to do that, so everything gets commented out... for now.
